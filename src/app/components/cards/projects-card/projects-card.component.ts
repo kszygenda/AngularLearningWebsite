d9596ee28195786fc9d7a1b88657b00189d0c358
kszygenda <rszygenda2002@gmail.com>
2025-04-02 20:09:45 +0200

    filled menu items

--- src/app/components/cards/projects-card/projects-card.component.ts
+++ src/app/components/cards/projects-card/projects-card.component.ts
@@ -11,6 +11,7 @@ import {MatList, MatListItem} from '@angular/material/list';
 import {MatIcon, MatIconModule} from '@angular/material/icon';
 import {NgForOf} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {cards} from './projects-list';
 
 @Component({
   selector: 'app-projects-card',
@@ -24,12 +25,22 @@ import {MatButtonModule} from '@angular/material/button';
     MatListItem,
     NgForOf,
     MatButtonModule,
-    MatCardImage
+    MatCardImage,
   ],
   standalone: true,
   templateUrl: './projects-card.component.html',
   styleUrl: './projects-card.component.css'
 })
 export class ProjectsCardComponent {
-
+  protected readonly cards = cards;
+  i: number = 0;
+  get selected_card() {
+    return cards[this.i];
+  }
+  goUpSelected(){
+    this.i = this.i == 2 ? this.i + 1 : this.i;
+  }
+  goDownSelected(){
+    this.i = this.i == 0 ? 0 : this.i - 1;
+  }
 }
