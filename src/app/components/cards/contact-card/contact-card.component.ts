54e9583a42337dccb8c8f0a60ac219b5d8d480ac
kszygenda <rszygenda2002@gmail.com>
2025-04-02 20:31:36 +0200

    finalizacja przed rozmową (brak reaktywnosci :| )

--- src/app/components/cards/contact-card/contact-card.component.ts
+++ src/app/components/cards/contact-card/contact-card.component.ts
@@ -1,12 +1,37 @@
+// contact-card.component.ts
 import { Component } from '@angular/core';
+import {
+  MatCard,
+  MatCardAvatar,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+} from "@angular/material/card";
+import {MatList, MatListItem} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-contact-card',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatList,
+    MatListItem,
+    MatIconModule,
+    NgForOf
+  ],
   standalone: true,
   templateUrl: './contact-card.component.html',
   styleUrl: './contact-card.component.css'
 })
 export class ContactCardComponent {
-
+  contactInfo = [
+    { icon: 'email', text: 'kszygenda12@gmail.com', link: 'mailto:kszygenda12@gmail.com' },
+    { icon: 'phone', text: '+48 660 599 699', link: 'tel:+48660599699' },
+    { icon: 'link', text: 'LinkedIn', link: 'https://linkedin.com/in/kszygenda' },
+    { icon: 'code', text: 'GitHub', link: 'https://github.com/kszygenda' }
+  ];
 }
