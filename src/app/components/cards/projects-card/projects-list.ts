037d64d82dfac0cb428d00ddd7cefbb23443aec9
kszygenda <rszygenda2002@gmail.com>
2025-04-02 14:29:02 +0200

    projects list type + bufor do uzupelnienia

---
+++ src/app/components/cards/projects-card/projects-list.ts
@@ -0,0 +1,33 @@
+type ProjectMenuItem = {
+  title: string,
+  description: string,
+  photo: string,
+  languages: Array<string>,
+}
+
+export const cards: ProjectMenuItem[] = [
+  {
+    title: "a",
+    description: "b",
+    photo: 'c',
+    languages: ['cpp'],
+  },
+  {
+    title: "a",
+    description: "b",
+    photo: 'c',
+    languages: ['cpp'],
+  },
+  {
+    title: "a",
+    description: "b",
+    photo: 'c',
+    languages: ['cpp'],
+  },
+  {
+    title: "a",
+    description: "b",
+    photo: 'c',
+    languages: ['cpp'],
+  },
+]
